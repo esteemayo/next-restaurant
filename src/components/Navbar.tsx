01d4e740dbd1c675d8895b9ee5cb48edad0c9574
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 05:36:47 +0100

    fix: add styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,18 @@
+import Link from 'next/link';
+
+import Menu from './Menu';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-red-500 uppercase'>
+      <div className='text-xl'>
+        <Link href='/'>Massimo</Link>
+      </div>
+      <div>
+        <Menu />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
