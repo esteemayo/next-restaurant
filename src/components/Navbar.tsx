b6b2494111983b12b8756a4f7ed60551b6b3f650
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 15:16:22 +0100

    fix: add styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   const user = false;
 
   return (
-    <nav className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-red-500 uppercase'>
-      <ul className='hidden list-none md:flex gap-4 items-center'>
+    <nav className='h-12 md:h-24 text-red-500 p-4 flex items-center justify-between border-b-2 border-red-500 uppercase'>
+      <ul className='hidden list-none md:flex gap-4 items-center flex-1'>
         <li>
           <Link href='/'>Home</Link>
         </li>
@@ -20,13 +20,13 @@ const Navbar = () => {
           <Link href='/'>Contact</Link>
         </li>
       </ul>
-      <div className='text-xl'>
+      <div className='text-xl md:font-bold flex-1 md:text-center'>
         <Link href='/'>Massimo</Link>
       </div>
       <div className='md:hidden'>
         <Menu />
       </div>
-      <ul className='hidden list-none md:flex gap-4 items-center'>
+      <ul className='hidden list-none md:flex gap-4 items-center flex-1'>
         <li className='flex items-center gap-2 px-1 bg-orange-300 rounded-md cursor-pointer'>
           <Image src='/img/phone.png' width={20} height={20} alt='phone' />
           <span>123 456 78</span>
