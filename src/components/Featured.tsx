3ada4c3193ba2e61b2f1faec843d6abf1f877c54
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 23:38:29 +0100

    fix: add styling

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -12,16 +12,18 @@ const Featured = () => {
           return (
             <div
               key={id}
-              className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300'
+              className='w-screen md:w-[50vw] xl:w-[33vw] h-[60vh] xl:h-[90vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300'
             >
               {img && (
                 <div className='relative flex-1 w-full p-4 hover:rotate-[60deg] transition-all duration-500'>
                   <Image src={img} fill alt='' className='object-contain' />
                 </div>
               )}
-              <div className='flex-1 flex flex-col items-center gap-4'>
-                <h1 className='text-xl font-bold uppercase'>{title}</h1>
-                <p className='p-4 text-center'>{desc}</p>
+              <div className='flex-1 flex flex-col items-center justify-center gap-4 text-center'>
+                <h1 className='text-xl lg:text-2xl 2xl:text-3xl font-bold uppercase'>
+                  {title}
+                </h1>
+                <p className='p-4 2xl:p-8 text-center'>{desc}</p>
                 <span className='text-xl font-bold'>
                   {formatCurrency(price)}
                 </span>
