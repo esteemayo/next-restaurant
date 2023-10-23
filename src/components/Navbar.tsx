4d39af8c3e1dc514c1fafb4d2d8891e5243d6170
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 14:56:32 +0100

    fix: add markup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,21 @@ import Menu from './Menu';
 const Navbar = () => {
   return (
     <nav className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-red-500 uppercase'>
+      <ul className='hidden list-none md:flex gap-4 items-center'>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <Link href='/menu'>Menu</Link>
+        </li>
+        <li>
+          <Link href='/'>Contact</Link>
+        </li>
+      </ul>
       <div className='text-xl'>
         <Link href='/'>Massimo</Link>
       </div>
-      <div>
+      <div className='md:hidden'>
         <Menu />
       </div>
     </nav>
