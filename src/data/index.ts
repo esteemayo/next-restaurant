b2280db96869f3aad62f0a6ac86affcfc5079bf5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 05:45:58 +0100

    feat: create menuLinks data array

---
+++ src/data/index.ts
@@ -0,0 +1,22 @@
+export const menuLinks = [
+  {
+    id: 1,
+    title: 'Home',
+    url: '/',
+  },
+  {
+    id: 2,
+    title: 'Menu',
+    url: '/menu',
+  },
+  {
+    id: 3,
+    title: 'Working hours',
+    url: '/',
+  },
+  {
+    id: 4,
+    title: 'Contact',
+    url: '/',
+  },
+];
