483ce8260e25e1ba061950823924e234069401a1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 15:08:01 +0100

    fix: add markup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link';
 
 import Menu from './Menu';
+import CartIcon from './CartIcon';
+import Image from 'next/image';
 
 const Navbar = () => {
+  const user = false;
+
   return (
     <nav className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-red-500 uppercase'>
       <ul className='hidden list-none md:flex gap-4 items-center'>
@@ -22,6 +26,24 @@ const Navbar = () => {
       <div className='md:hidden'>
         <Menu />
       </div>
+      <ul className='hidden list-none md:flex gap-4 items-center'>
+        <li className='flex items-center gap-2 px-1 bg-orange-300 rounded-md cursor-pointer'>
+          <Image src='/img/phone.png' width={20} height={20} alt='phone' />
+          <span>123 456 78</span>
+        </li>
+        {!user ? (
+          <li>
+            <Link href='/'>Login</Link>
+          </li>
+        ) : (
+          <li>
+            <Link href='/menu'>Orders</Link>
+          </li>
+        )}
+        <li>
+          <CartIcon />
+        </li>
+      </ul>
     </nav>
   );
 };
