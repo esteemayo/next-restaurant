1be7bbea03f4edcdf83f1069e5568eac15881475
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 23:59:17 +0100

    fix: add styling

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 const Offer = () => {
   return (
     <div className='h-screen w-screen bg-black text-white flex flex-col md:flex-row'>
-      <div className='flex-1 flex flex-col items-center justify-center'>
+      <div className='flex-1 flex flex-col items-center justify-center gap-8 text-center p-6'>
         <h1 className='text-inherit text-5xl xl:text-6xl font-bold capitalize'>
           Delicious burger & french fry
         </h1>
-        <p className='text-inherit'>
+        <p className='text-inherit xl:text-xl'>
           Progressively simplify effective e-toilers and process-centric methods
           of empowerment. Quickly pontificate parallel.
         </p>
