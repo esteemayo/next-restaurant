a68932d97cf7e8f88e055b8d593d976afc50d3ac
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 05:21:09 +0100

    fix: add markup

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,6 +1,6 @@
 const Notification = () => {
   return (
-    <section className='h-12 bg-rose-500 text-white px-4 flex items-center justify-center text-center'>
+    <section className='h-12 bg-rose-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>
       Free delivery for all orders over $50. Order your food now!
     </section>
   );
