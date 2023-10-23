aacf4c6ff3048dde0bec1899ae2be1a72a108b77
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 05:26:57 +0100

    fix: add markup

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,11 @@
+import Image from 'next/image';
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <aside>
+      <Image src='/img/open.png' width={20} height={20} alt='open' />
+    </aside>
+  );
 };
 
 export default Menu;
