729c0e48d9496b2374ea055f6f5c509c94058385
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 15:08:53 +0100

    fix: add styling

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const CartIcon = () => {
   return (
     <Link href='/cart' className='flex items-center gap-4'>
-      <div className='relative w-8 h-8'>
+      <div className='relative w-8 h-8 md:w-5 md:h-5'>
         <Image src='/img/cart.png' fill alt='cart' />
       </div>
       <span>Cart (3)</span>
