82097198a8df97b31403c61574183888e7a2ec58
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 15:44:56 +0100

    fix: add markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import Featured from '@/components/Featured';
+import Offer from '@/components/Offer';
+import Slider from '@/components/Slider';
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+      <Slider />
+      <Featured />
+      <Offer />
+    </main>
   );
 };
 
