f0881cbbdfdd46299bf85d98fc07f95d7647a4f9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 14:49:25 +0100

    fix: add markup

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,10 +4,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 
+import CartIcon from './CartIcon';
 import { menuLinks } from '@/data';
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const user = false;
 
   return (
     <div className='cursor-pointer'>
@@ -28,16 +31,28 @@ const Menu = () => {
           onClick={() => setIsOpen(false)}
         />
       )}
-      <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10'>
-        {menuLinks.map((link) => {
-          const { id, title, url } = link;
-          return (
-            <Link key={id} href={url}>
-              {title}
+      {isOpen && (
+        <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10'>
+          {menuLinks.map((link) => {
+            const { id, title, url } = link;
+            return (
+              <Link key={id} href={url} onClick={() => setIsOpen(false)}>
+                {title}
+              </Link>
+            );
+          })}
+          {!user ? (
+            <Link href='/login' onClick={() => setIsOpen(false)}>
+              Login
             </Link>
-          );
-        })}
-      </div>
+          ) : (
+            <Link href='/orders' onClick={() => setIsOpen(false)}>
+              Orders
+            </Link>
+          )}
+          <CartIcon />
+        </div>
+      )}
     </div>
   );
 };
