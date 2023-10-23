c46d7442bd19a871d82ab88a971569505d9e965f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 23:40:41 +0100

    fix: add styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className={`${inter.className} overflow-x-hidden`}>
         <Layout>{children}</Layout>
       </body>
     </html>
