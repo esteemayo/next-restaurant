ceea619a6be45529b28bff31f88a264fe60c3819
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 19:18:40 +0100

    fix: add markup

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,5 +1,28 @@
+import Image from 'next/image';
+
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div className='w-screen overflow-x-scroll text-red-500'>
+      <div className='w-max flex'>
+        <div className='w-screen h-[60vh] flex flex-col items-center justify-around'>
+          <div className='relative flex-1 w-full p-4'>
+            <Image
+              src='/img/temporary/p1.png'
+              fill
+              alt=''
+              className='object-contain'
+            />
+          </div>
+          <div className='flex-1'>
+            <h1 className=''>title</h1>
+            <p>desc</p>
+            <span>price</span>
+            <button>add to cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
