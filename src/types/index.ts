2ea3c3c2f96f80726c0ef7df3e7d5eea22a61173
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 22:36:34 +0100

    feat: add Product type annotation

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,12 @@
 export interface LayoutProps {
   children: React.ReactNode;
 }
+
+export interface Product {
+  id: number;
+  title: string;
+  desc?: string;
+  img?: string;
+  price: number;
+  options?: { title: string; additionalPrice: number }[];
+}
