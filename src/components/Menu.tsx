78bb9ebb9f28a3f9bbcb0b503a495db265392e53
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 05:43:41 +0100

    fix: add markup

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,30 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 
 const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      <Image src='/img/open.png' width={20} height={20} alt='open' />
+    <div className='cursor-pointer'>
+      {!isOpen ? (
+        <Image
+          src='/img/open.png'
+          width={20}
+          height={20}
+          alt='open'
+          onClick={() => setIsOpen(true)}
+        />
+      ) : (
+        <Image
+          src='/img/close.png'
+          width={20}
+          height={20}
+          alt='close'
+          onClick={() => setIsOpen(false)}
+        />
+      )}
     </div>
   );
 };
