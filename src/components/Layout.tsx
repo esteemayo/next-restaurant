048bc5ae2c4b520206f789d05b1498586da4e78c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 05:07:47 +0100

    fix: add components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,20 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { FC } from 'react';
+
+import Navbar from './Navbar';
+import Notification from './Notification';
+import Footer from './Footer';
+
+import { LayoutProps } from '@/types';
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Notification />
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
