a42608ec68da072ace2362b90b3c52a197a0539c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 15:40:08 +0100

    fix: add markup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between uppercase'>
+      <Link href='/' className='font-bold text-xl'>
+        Massimo
+      </Link>
+      <span>&copy; All rights reserved</span>
+    </footer>
+  );
 };
 
 export default Footer;
