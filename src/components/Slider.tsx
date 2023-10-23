035dc43ffc14d0ff2587599dc762742287257edc
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 16:02:40 +0100

    fix: add styling

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -3,16 +3,18 @@ import Image from 'next/image';
 const Slider = () => {
   return (
     <div className='flex flex-col lg:flex-row h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
-      <div className='h-1/2'>
-        <h1 className=''>Texts</h1>
-        <button className=''>Order now</button>
+      <div className='h-1/2 flex flex-col items-center justify-center gap-8 text-red-500 font-bold'>
+        <h1 className='text-5xl text-center uppercase p-4'>Texts</h1>
+        <button className='py-4 px-8 bg-red-500 text-white capitalize'>
+          Order now
+        </button>
       </div>
       <div className='w-full h-1/2 relative'>
         <Image
           src='/img/slide1.png'
           fill
           alt='slide'
-          className='object-cover'
+          className='block object-cover'
         />
       </div>
     </div>
