93a494ab5e75a27ee787fb5a4aafaa3aaf06c706
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-23 18:54:14 +0100

    fix: add styling

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,17 +1,38 @@
+'use client';
+
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+import { sliderItems } from '@/data';
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = sliderItems.length - 1;
+
+    const interval = setInterval(() => {
+      setCurrentSlide((value) => {
+        return value === lastIndex ? 0 : value + 1;
+      });
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className='flex flex-col lg:flex-row h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
       <div className='h-1/2 flex flex-col items-center justify-center gap-8 text-red-500 font-bold'>
-        <h1 className='text-5xl text-center uppercase p-4'>Texts</h1>
+        <h1 className='text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl'>
+          {sliderItems[currentSlide].title}
+        </h1>
         <button className='py-4 px-8 bg-red-500 text-white capitalize'>
           Order now
         </button>
       </div>
       <div className='w-full h-1/2 relative'>
         <Image
-          src='/img/slide1.png'
+          src={sliderItems[currentSlide].image}
           fill
           alt='slide'
           className='block object-cover'
