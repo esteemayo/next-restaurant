9ae151ff458870ac1a7e5cc3cf8bed984fc7edf0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 02:37:40 +0100

    fix: add styling

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -5,18 +5,18 @@ const Menu = () => {
   return (
     <main className='p-4 lg:px-20 xl:p-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
       {menu.map((item) => {
-        const { id, img, slug, desc, title } = item;
+        const { id, img, slug, desc, title, color } = item;
         return (
           <Link
             key={id}
-            href={slug}
-            className='w-full h-1/3 bg-cover'
+            href={`/menu/${encodeURIComponent(slug)}`}
+            className='w-full h-1/3 bg-cover p-8 md:h-1/2'
             style={{ backgroundImage: `url(${img})` }}
           >
-            <div className=''>
-              <h1>{title}</h1>
-              <p>{desc}</p>
-              <button className=''>Explore</button>
+            <div className={`text-${color} w-1/2`}>
+              <h1 className='uppercase font-bold text-3xl'>{title}</h1>
+              <p className='text-sm my-8'>{desc}</p>
+              <button className={`hidden 2xl:block capitalize`}>Explore</button>
             </div>
           </Link>
         );
