346586eb5fab73cd93bdffec553187e1369a8e76
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 01:59:57 +0100

    fix: add styling

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,7 +1,16 @@
 'use client';
 
+import Countdown from 'react-countdown';
+
+const endingDate = new Date('2023-12-31');
+
 const CountDown = () => {
-  return <div>CountDown</div>;
+  return (
+    <Countdown
+      className='font-bold text-5xl text-yellow-300'
+      date={endingDate}
+    />
+  );
 };
 
 export default CountDown;
