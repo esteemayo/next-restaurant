9fba952f24cad716af21da36d4c892d5fc647447
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 02:10:11 +0100

    fix: add styling

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -7,7 +7,7 @@ const endingDate = new Date('2023-12-31');
 const CountDown = () => {
   return (
     <Countdown
-      className='font-bold text-5xl text-yellow-300'
+      className='font-bold tabular-nums text-5xl text-yellow-300'
       date={endingDate}
     />
   );
