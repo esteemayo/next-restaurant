9ca58d5daf750eb97b0eb13cbbd8ed1041a9e6b1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 15:16:50 +0100

    fix: add styling

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -21,16 +21,18 @@ const Price: FC<PriceProps> = ({ id, price, options }) => {
           );
         })}
       </div>
-      <div className=''>
-        <div className=''>
-          <span className=''>Quantity</span>
-          <div className=''>
-            <button className=''>{'<'}</button>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center justify-between w-full p-3 ring-1 ring-red-500'>
+          <span>Quantity</span>
+          <div className='flex items-center gap-4'>
+            <button>{'<'}</button>
             <span>1</span>
-            <button className=''>{'>'}</button>
+            <button>{'>'}</button>
           </div>
         </div>
-        <button className=''>Add to Cart</button>
+        <button className='w-56 uppercase bg-red-500 text-white p-3 ring-1 ring-red-500 outline-red-400 rounded-sm'>
+          Add to Cart
+        </button>
       </div>
     </div>
   );
