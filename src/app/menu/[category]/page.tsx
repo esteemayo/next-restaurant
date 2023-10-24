7922bace59e40950a235ab2946a2be765905a1e9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 03:42:34 +0100

    fix: add styling

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -5,18 +5,18 @@ import { pizzas } from '@/data';
 
 const Category = () => {
   return (
-    <main className='flex text-red-500'>
+    <main className='flex flex-wrap text-red-500'>
       {pizzas.map((item) => {
         const { id, price, title, desc, img, options } = item;
         return (
           <Link
             key={id}
             href={`/product/${encodeURIComponent(id)}`}
-            className='w-full h-[60vh] border-r-2 border-b-2 border-red-500'
+            className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 md:w-1/3 p-4'
           >
             {img && (
-              <div className='relative'>
-                <Image src={img} fill alt='image' />
+              <div className='relative h-[80%]'>
+                <Image src={img} fill alt='image' className='object-contain' />
               </div>
             )}
           </Link>
