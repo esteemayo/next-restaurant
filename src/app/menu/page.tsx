2b743ddf1b6f96614c430844c0e35e19aba21676
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 03:09:30 +0100

    fix: add styling

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -10,7 +10,7 @@ const Menu = () => {
           <Link
             key={id}
             href={`/menu/${encodeURIComponent(slug)}`}
-            className='w-full h-1/3 bg-cover p-8 md:h-1/2'
+            className='w-full h-1/3 bg-cover p-8 md:h-[1/2] xl:h-[100%] outline-none'
             style={{ backgroundImage: `url(${img})` }}
           >
             <div className={`text-${color} w-1/2`}>
@@ -19,7 +19,9 @@ const Menu = () => {
               <button
                 className={`hidden 2xl:block capitalize py-2 px-4 bg-${color} text-${
                   color === 'black' ? 'white' : 'red-500'
-                } rounded-sm`}
+                } rounded-sm outline-${
+                  color === 'black' ? 'white' : 'red-400'
+                }`}
               >
                 Explore
               </button>
