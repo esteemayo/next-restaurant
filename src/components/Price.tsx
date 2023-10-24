d3312261d73c5d3b6d46879c094bb8d9600e4e16
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 04:48:28 +0100

    fix: add markup

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,7 +1,27 @@
 'use client';
 
-const Price = () => {
-  return <div>Price</div>;
+import { FC } from 'react';
+
+import { PriceProps } from '@/types';
+import { formatCurrency } from '@/utils/formatCurrency';
+
+const Price: FC<PriceProps> = ({ id, price, options }) => {
+  return (
+    <div className=''>
+      <h2 className=''>{formatCurrency(parseFloat(price.toFixed(2)))}</h2>
+      <div className=''>
+        {options?.map((option) => {
+          const { title, additionalPrice } = option;
+          return (
+            <button key={title} className=''>
+              {title}
+            </button>
+          );
+        })}
+      </div>
+      <div className=''></div>
+    </div>
+  );
 };
 
 export default Price;
