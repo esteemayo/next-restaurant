6850dfb18c3c13d30c55ba88717de04d40f623e4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 21:25:57 +0100

    fix: add styling

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -5,9 +5,9 @@ import { singleProduct } from '@/data';
 
 const Product = () => {
   return (
-    <main className='p-4 lg:px-20 xl:p-40 h-screen flex flex-col justify-around text-red-500 md:flex-row'>
+    <main className='p-4 lg:px-20 xl:p-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center'>
       {singleProduct.img && (
-        <div className='relative w-full h-1/2'>
+        <div className='relative w-full h-1/2 md:h-[70%]'>
           <Image
             src={singleProduct.img}
             fill
@@ -16,8 +16,8 @@ const Product = () => {
           />
         </div>
       )}
-      <div className='h-1/2 flex flex-col gap-4'>
-        <h1 className='text-3xl font-bold uppercase'>{singleProduct.title}</h1>
+      <div className='h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8'>
+        <h1 className='text-3xl font-bold uppercase xl:text-5xl'>{singleProduct.title}</h1>
         <p className=''>{singleProduct.desc}</p>
         <Price
           id={singleProduct.id}
