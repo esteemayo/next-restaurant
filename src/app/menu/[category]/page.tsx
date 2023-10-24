75045a5fb8cce9d50d42a442e8c62cb743e44214
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 03:35:44 +0100

    fix: add markup

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,5 +1,29 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { pizzas } from '@/data';
+
 const Category = () => {
-  return <div>Category</div>;
+  return (
+    <main className='flex text-red-500'>
+      {pizzas.map((item) => {
+        const { id, price, title, desc, img, options } = item;
+        return (
+          <Link
+            key={id}
+            href={`/product/${encodeURIComponent(id)}`}
+            className='w-full h-[60vh] border-r-2 border-b-2 border-red-500'
+          >
+            {img && (
+              <div className='relative'>
+                <Image src={img} fill alt='image' />
+              </div>
+            )}
+          </Link>
+        );
+      })}
+    </main>
+  );
 };
 
 export default Category;
