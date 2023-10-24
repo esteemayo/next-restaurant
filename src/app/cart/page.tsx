d824aa54f51187bf33cd8c163677b141d849ea1f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 22:28:49 +0100

    fix: add markup

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,22 @@
+import Image from 'next/image';
+
+import { formatCurrency } from '@/utils/formatCurrency';
+
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <main className='p-4 lg:p-20 xl:p-40'>
+      <div className='relative'>
+        <Image src='/img/temporary/p1.png' fill alt='' />
+        <div className=''>
+          <h1 className=''>sicilian</h1>
+          <span className=''>Large</span>
+        </div>
+        <h2 className=''>{formatCurrency(79.9)}</h2>
+        <span>X</span>
+      </div>
+      <div className=''></div>
+    </main>
+  );
 };
 
 export default Cart;
