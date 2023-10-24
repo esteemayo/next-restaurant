11e3bcf59e7d4d03c9199ab33a3bafd22b85fb31
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 03:20:01 +0100

    fix: fix hydration error

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,16 +4,17 @@ import Navbar from './Navbar';
 import Notification from './Notification';
 import Footer from './Footer';
 
+import ClientOnly from './ClientOnly';
 import { LayoutProps } from '@/types';
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <>
+    <ClientOnly>
       <Notification />
       <Navbar />
       {children}
       <Footer />
-    </>
+    </ClientOnly>
   );
 };
 
