d138f9e3dfdb8b54168b8ff72b7bc16b58d8b921
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 04:31:12 +0100

    fix: add markup

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,21 @@
+import Image from 'next/image';
+
+import { singleProduct } from '@/data';
+
 const Product = () => {
-  return <div>Product</div>;
+  return (
+    <main className='p-4 lg:px-20 xl:p-40 h-screen flex flex-col justify-around text-red-500 md:flex-row'>
+      {singleProduct.img && (
+        <div className='relative'>
+          <Image src={singleProduct.img} fill alt='' />
+        </div>
+      )}
+      <div className=''>
+        <h1 className=''>{singleProduct.title}</h1>
+        <p className=''>{singleProduct.desc}</p>
+      </div>
+    </main>
+  );
 };
 
 export default Product;
