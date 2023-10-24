4bc0d4ab38ea69b73ad54b09898b716ea03edc75
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 03:53:01 +0100

    fix: add styling

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -13,15 +13,15 @@ const Category = () => {
           <Link
             key={id}
             href={`/product/${encodeURIComponent(id)}`}
-            className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 md:w-1/3 p-4'
+            className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 md:w-1/3 p-4 flex flex-col justify-between'
           >
             {img && (
               <div className='relative h-[80%]'>
                 <Image src={img} fill alt='image' className='object-contain' />
               </div>
             )}
-            <div className='flex items-center justify-between'>
-              <h1 className=''>{title}</h1>
+            <div className='flex items-center justify-between font-bold'>
+              <h1 className='text-2xl uppercase p-2'>{title}</h1>
               <h2 className=''>{formatCurrency(price)}</h2>
               <button className='uppercase bg-red-500 text-white p-2 rounded-sm'>
                 Add to cart
