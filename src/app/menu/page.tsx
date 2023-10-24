d6941d14593c99dacfce02368b4cc4cfcf188fd5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 02:27:59 +0100

    fix: add markup

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,5 +1,28 @@
+import { menu } from '@/data';
+import Link from 'next/link';
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <main className='p-4 lg:px-20 xl:p-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
+      {menu.map((item) => {
+        const { id, img, slug, desc, title } = item;
+        return (
+          <Link
+            key={id}
+            href={slug}
+            className='w-full h-1/3 bg-cover'
+            style={{ backgroundImage: `url(${img})` }}
+          >
+            <div className=''>
+              <h1>{title}</h1>
+              <p>{desc}</p>
+              <button className=''>Explore</button>
+            </div>
+          </Link>
+        );
+      })}
+    </main>
+  );
 };
 
 export default Menu;
