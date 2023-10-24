02f3fa6cea8a52a79c4dc145cb20d6dc09d00046
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-24 02:07:17 +0100

    fix: add styling

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -4,7 +4,7 @@ import CountDown from './CountDown';
 
 const Offer = () => {
   return (
-    <div className='h-screen w-screen bg-black text-white flex flex-col md:flex-row'>
+    <div className='h-screen w-screen bg-black text-white flex flex-col md:flex-row md:justify-between md:bg-[url(/img/offerBg.png)] md:h-[70vh]'>
       <div className='flex-1 flex flex-col items-center justify-center gap-8 text-center p-6'>
         <h1 className='text-inherit text-5xl xl:text-6xl font-bold capitalize'>
           Delicious burger & french fry
@@ -18,7 +18,7 @@ const Offer = () => {
           Order now
         </button>
       </div>
-      <div className='flex-1 w-full relative'>
+      <div className='flex-1 w-full md:h-full relative'>
         <Image
           src='/img/offerProduct.png'
           fill
