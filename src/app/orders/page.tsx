e5fc82e97be3454b6f18164bb7e9d2b019ae2713
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-25 05:24:35 +0100

    fix: add markup

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,5 +1,28 @@
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <main className='p-4 lg:p-20 xl:p-40'>
+      <table className=''>
+        <thead>
+          <tr>
+            <th>Order ID</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th>Products</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1237861238721</td>
+            <td>19.07.2023</td>
+            <td>89.90</td>
+            <td>Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+            <td>On the way (approx. 10min)...</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  );
 };
 
 export default Orders;
