fb182561d0c2457908a819d55d381075612b833e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-25 04:28:33 +0100

    fix: add styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,10 +5,10 @@ const Login = () => {
   return (
     <main className='p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center'>
       <div className='h-full shadow-2xl rounded-md flex flex-col md:flex-row md:w-full md:h-[70%]'>
-        <div className='relative w-full h-1/3'>
+        <div className='relative w-full h-1/3 md:w-1/2 md:h-full'>
           <Image src='/img/loginBg.webp' fill alt='' className='object-cover' />
         </div>
-        <div className='p-10 flex flex-col gap-8'>
+        <div className='p-10 flex flex-col gap-8 md:w-1/2'>
           <h1 className='font-bold text-xl'>Welcome</h1>
           <p className=''>
             Log into your account or create a new one using social buttons
