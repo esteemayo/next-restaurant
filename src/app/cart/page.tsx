463b1646baa4df53c0a5c434dae951f0aca265ac
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-25 03:41:37 +0100

    fix: refactor

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,38 +1,26 @@
 import Image from 'next/image';
 
 import { formatCurrency } from '@/utils/formatCurrency';
+import { cartItems } from '@/data';
 
 const Cart = () => {
   return (
     <main className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row'>
       <div className='h-1/2 p-4 flex flex-col justify-center overflow-y-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:p-20 xl:p-40'>
-        <div className='flex items-center justify-between mb-4'>
-          <Image src='/img/temporary/p1.png' width={100} height={100} alt='' />
-          <div className=''>
-            <h1 className='uppercase text-xl font-bold'>sicilian</h1>
-            <span className=''>Large</span>
-          </div>
-          <h2 className='font-bold'>{formatCurrency(79.9)}</h2>
-          <span className='cursor-pointer'>X</span>
-        </div>
-        <div className='flex items-center justify-between mb-4'>
-          <Image src='/img/temporary/p1.png' width={100} height={100} alt='' />
-          <div className=''>
-            <h1 className='uppercase text-xl font-bold'>sicilian</h1>
-            <span className=''>Large</span>
-          </div>
-          <h2 className='font-bold'>{formatCurrency(79.9)}</h2>
-          <span className='cursor-pointer'>X</span>
-        </div>
-        <div className='flex items-center justify-between mb-4'>
-          <Image src='/img/temporary/p1.png' width={100} height={100} alt='' />
-          <div className=''>
-            <h1 className='uppercase text-xl font-bold'>sicilian</h1>
-            <span className=''>Large</span>
-          </div>
-          <h2 className='font-bold'>{formatCurrency(79.9)}</h2>
-          <span className='cursor-pointer'>X</span>
-        </div>
+        {cartItems.map((item) => {
+          const { id, img, title, price, options } = item;
+          return (
+            <div key={id} className='flex items-center justify-between mb-4'>
+              <Image src={img} width={100} height={100} alt='' />
+              <div className=''>
+                <h1 className='uppercase text-xl font-bold'>{title}</h1>
+                <span className=''>{options.title}</span>
+              </div>
+              <h2 className='font-bold'>{formatCurrency(price)}</h2>
+              <span className='cursor-pointer'>X</span>
+            </div>
+          );
+        })}
       </div>
       <div className='h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:p-20 xl:p-40 2xl:text-xl 2xl:gap-6'>
         <div className='flex justify-between'>
