bfc9a5ba39964645b72a2530aae1cc80aea96918
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-25 16:17:17 +0100

    fix: add styling

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -12,7 +12,7 @@ const Orders = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className='text-sm md:text-base odd:bg-gray-100'>
+          <tr className='text-sm md:text-base bg-red-50'>
             <td className='hidden md:block py-6 px-1'>1237861238721</td>
             <td className='py-6 px-1'>19.07.2023</td>
             <td className='py-6 px-1'>89.90</td>
