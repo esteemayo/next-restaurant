31a48bf6f4c1601c00d90726f373b5bd0c3f8676
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-25 21:22:23 +0100

    fix: change directory

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import Menu from '../Menu';
+import CartIcon from '../CartIcon';
+
+const Navbar = () => {
+  const user = false;
+
+  return (
+    <nav className='h-12 md:h-24 text-red-500 p-4 flex items-center justify-between border-b-2 border-red-500 uppercase lg:px-20 xl:px-40'>
+      <ul className='hidden list-none md:flex gap-4 items-center flex-1'>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <Link href='/menu'>Menu</Link>
+        </li>
+        <li>
+          <Link href='/'>Contact</Link>
+        </li>
+      </ul>
+      <div className='text-xl md:font-bold flex-1 md:text-center'>
+        <Link href='/'>Massimo</Link>
+      </div>
+      <div className='md:hidden'>
+        <Menu />
+      </div>
+      <ul className='hidden list-none md:flex gap-4 items-center justify-end flex-1'>
+        <li className='md:absolute top-3 right-2 lg:static flex items-center gap-2 px-1 bg-orange-300 rounded-md cursor-pointer'>
+          <Image src='/img/phone.png' width={20} height={20} alt='phone' />
+          <span>123 456 78</span>
+        </li>
+        {!user ? (
+          <li>
+            <Link href='/login'>Login</Link>
+          </li>
+        ) : (
+          <li>
+            <Link href='/menu'>Orders</Link>
+          </li>
+        )}
+        <li>
+          <CartIcon />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
