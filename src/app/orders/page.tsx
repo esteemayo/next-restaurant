477527e202003ee994b49b808a4a8a7ebee79a6e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-25 16:15:41 +0100

    fix: add markup

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -21,6 +21,24 @@ const Orders = () => {
             </td>
             <td>On the way (approx. 10min)...</td>
           </tr>
+          <tr className='text-sm md:text-base odd:bg-gray-100'>
+            <td className='hidden md:block py-6 px-1'>1237861238721</td>
+            <td className='py-6 px-1'>19.07.2023</td>
+            <td className='py-6 px-1'>89.90</td>
+            <td className='hidden md:block py-6 px-1'>
+              Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+            </td>
+            <td>On the way (approx. 10min)...</td>
+          </tr>
+          <tr className='text-sm md:text-base odd:bg-gray-100'>
+            <td className='hidden md:block py-6 px-1'>1237861238721</td>
+            <td className='py-6 px-1'>19.07.2023</td>
+            <td className='py-6 px-1'>89.90</td>
+            <td className='hidden md:block py-6 px-1'>
+              Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)
+            </td>
+            <td>On the way (approx. 10min)...</td>
+          </tr>
         </tbody>
       </table>
     </main>
