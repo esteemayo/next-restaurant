96b8cf7a9be37288a6e60aca83ae6ccb3500e591
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-25 04:39:50 +0100

    fix: add styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Login = () => {
   return (
     <main className='p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center'>
-      <div className='h-full shadow-2xl rounded-md flex flex-col md:flex-row md:w-full md:h-[70%]'>
+      <div className='h-full shadow-2xl rounded-md flex flex-col md:flex-row md:w-full lg:w-[70%] 2xl:w-[50%]'>
         <div className='relative w-full h-1/3 md:w-1/2 md:h-full'>
           <Image src='/img/loginBg.webp' fill alt='' className='object-cover' />
         </div>
