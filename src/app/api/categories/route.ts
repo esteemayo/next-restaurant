b11593835972024537da9b82527664299bf15c48
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-06 04:34:18 +0100

    feat: fetch all categories

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,5 +1,17 @@
 import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
 
-export const GET = (req: Request, res: Response) => {
-  return new NextResponse('hello', { status: 200 });
+const prisma = new PrismaClient();
+
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (err) {
+    return new NextResponse(
+      JSON.stringify({ message: 'Something went wrong!' }),
+      { status: 500 }
+    );
+  }
 };
