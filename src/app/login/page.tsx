24485e7346698b89c865eb1cbc30b6d9a22971fd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-07 04:22:53 +0100

    fix: refactor

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,8 @@ import { signIn, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
+import LoginButtons from '@/components/LoginButtons';
+
 const Login = () => {
   const router = useRouter();
   const { status } = useSession();
@@ -13,7 +15,7 @@ const Login = () => {
     return <p>loading...</p>;
   }
 
-  if (status==='authenticated') {
+  if (status === 'authenticated') {
     return router.push('/');
   }
 
@@ -28,41 +30,7 @@ const Login = () => {
             className='block object-cover rounded-l-md'
           />
         </div>
-        <div className='p-10 flex flex-col gap-8 md:w-1/2'>
-          <h1 className='font-bold text-xl 2xl:text-3xl'>Welcome</h1>
-          <p className=''>
-            Log into your account or create a new one using social buttons
-          </p>
-          <button
-            className='flex items-center gap-4 p-4 ring-1 ring-orange-100 rounded-sm'
-            onClick={() => signIn('google')}
-          >
-            <Image
-              src='/img/google.png'
-              width={20}
-              height={20}
-              alt='google'
-              className='block object-contain'
-            />
-            <span className=''>Sign in with Google</span>
-          </button>
-          <button className='flex items-center gap-4 p-4 ring-1 ring-blue-100 rounded-sm'>
-            <Image
-              src='/img/facebook.png'
-              width={20}
-              height={20}
-              alt='facebook'
-              className='block object-contain'
-            />
-            <span className=''>Sign in with Facebook</span>
-          </button>
-          <p className='text-sm'>
-            Have a problem?{' '}
-            <Link href='/' className='underline underline-offset-2'>
-              Contact us
-            </Link>
-          </p>
-        </div>
+        <LoginButtons />
       </div>
     </main>
   );
