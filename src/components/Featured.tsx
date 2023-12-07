36923ea211f69aa5f560cd91336d4823bbe7056b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-07 01:58:54 +0100

    feat: fetch featured products from the api

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,7 +1,20 @@
-import { featuredProducts } from '@/data';
 import FeaturedCard from './cards/FeaturedCard';
 
-const Featured = () => {
+const getData = async () => {
+  const res = await fetch('http://localhost:3000/api/products');
+
+  if (!res.ok) {
+    throw new Error('Failed');
+  }
+
+  const data = await res.json();
+  return data;
+};
+
+const Featured = async () => {
+  const featuredProducts = await getData();
+  console.log(featuredProducts);
+
   return (
     <div className='w-screen overflow-x-scroll text-red-500'>
       <div className='w-max flex'>
