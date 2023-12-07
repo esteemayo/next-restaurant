cd7511ccce2ef50053d6b8dda274797354bba5eb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-07 17:54:00 +0100

    feat: add prisma adapter

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,12 @@
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
 import { NextAuthOptions, getServerSession } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+import { prisma } from './connect';
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
