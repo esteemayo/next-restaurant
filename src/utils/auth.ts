06d2a18c2367fe66ff29cba72ee9fa8adc982dde
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-07 02:56:41 +0100

    feat: add authOptions

---
+++ src/utils/auth.ts
@@ -0,0 +1,16 @@
+import FacebookProvider from 'next-auth/providers/facebook';
+import { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_SECRET!,
+    }),
+  ],
+};
