bd14ef7f0dbf76399a1b7dbe3cd9f76ad9c2c28d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-07 02:11:11 +0100

    fix: add prop type safety

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,7 +1,13 @@
 import { pizzas } from '@/data';
 import ProductCard from '@/components/cards/ProductCard';
 
-const Category = () => {
+interface CategoryProps {
+  params: {
+    category: string;
+  };
+}
+
+const Category = ({ params }: CategoryProps) => {
   return (
     <main className='flex flex-wrap text-red-500'>
       {pizzas.map((item) => {
