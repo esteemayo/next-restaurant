9033e00b65982af6b6d7a6ddea04072bb0a12656
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-07 02:15:27 +0100

    fix: fetch products from api

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,5 +1,20 @@
-import { pizzas } from '@/data';
 import ProductCard from '@/components/cards/ProductCard';
+
+const getData = async (category: string) => {
+  const res = await fetch(
+    `http://localhost:3000/api/products?category=${category}`,
+    {
+      cache: 'no-store',
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed');
+  }
+
+  const data = await res.json();
+  return data;
+};
 
 interface CategoryProps {
   params: {
@@ -7,7 +22,9 @@ interface CategoryProps {
   };
 }
 
-const Category = ({ params }: CategoryProps) => {
+const Category = async ({ params }: CategoryProps) => {
+  const pizzas = await getData(params.category);
+
   return (
     <main className='flex flex-wrap text-red-500'>
       {pizzas.map((item) => {
