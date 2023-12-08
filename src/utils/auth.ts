b63e7d0065e5b1eb694e73337d7d3c34aff15347
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-08 01:46:04 +0100

    feat: add callbacks

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,12 +1,29 @@
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
-import { NextAuthOptions, getServerSession } from 'next-auth';
+import { NextAuthOptions, User, getServerSession } from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 import { prisma } from './connect';
 
+declare module 'next-auth' {
+  interface Session {
+    user: User & {
+      isAdmin: Boolean;
+    };
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    isAdmin: Boolean;
+  }
+}
+
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
@@ -17,6 +34,24 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.FACEBOOK_SECRET!,
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user.isAdmin = token.isAdmin;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      const userInDb = await prisma.user.findUnique({
+        where: {
+          email: token.email!,
+        },
+      });
+      token.isAdmin = userInDb?.isAdmin!;
+      return token;
+    },
+  },
 };
 
 export const getAuthSession = () => getServerSession(authOptions);
