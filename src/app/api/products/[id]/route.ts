12fb5b34ce0ca4b4382f214dc8b4e7b26ac6d028
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-08 16:08:21 +0100

    feat: handle get single product api route logic

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+
+import { prisma } from '@/utils/connect';
+
+interface IParams {
+  params: {
+    id: string;
+  };
+}
+
+export const GET = async ({ params }: IParams) => {
+  const { id: productId } = params;
+  
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: productId
+      }
+    });
+
+    return new NextResponse(JSON.stringify(product), { status: 200 })
+  } catch (err) {
+    return new NextResponse(JSON.stringify({ message: 'Something went wrong!' }),
+      { status: 500 }
+    );
+  }
+}
