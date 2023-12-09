f50be2e771abec68ef9a70e19e0c05e4d1e39106
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-09 15:22:20 +0100

    feat: add useCartStore for cart product

---
+++ src/hooks/useCartStore.ts
@@ -0,0 +1,53 @@
+import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+import { produce } from 'immer';
+
+import { ActionType, CartItemType } from '@/types';
+
+interface CartStore {
+  products: CartItemType[];
+  totalItems: number;
+  totalPrice: number;
+}
+
+const INITIAL_STATE = {
+  products: [],
+  totalItems: 0,
+  totalPrice: 0,
+};
+
+export const useCartStore = create<CartStore & ActionType>()(
+  persist(
+    devtools((set) => ({
+      products: INITIAL_STATE.products,
+      totalItems: INITIAL_STATE.totalItems,
+      totalPrice: INITIAL_STATE.totalPrice,
+      addToCart: (payload) =>
+        set(
+          produce((state) => {
+            state.products.push(payload);
+            state.totalItems = state.totalItems + payload.quantity;
+            state.totalPrice = state.totalPrice * payload.quantity;
+          }),
+          false,
+          'addToCart'
+        ),
+      removeFromCart: (payload) =>
+        set(
+          produce((state) => {
+            state.products = state.products.splice(
+              state.products.find(
+                (item: { id: string }) => item.id !== payload.id
+              ),
+              1
+            );
+            state.totalItems = state.totalItems - payload.quantity;
+            state.totalPrice = state.totalPrice * payload.quantity;
+          }),
+          false,
+          'removeFromCart'
+        ),
+    })),
+    { name: 'cart' }
+  )
+);
