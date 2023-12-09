ec3783035266544b0b9f304c6318c2fda15222f3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-10 00:59:39 +0100

    fix: refactor

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,17 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-interface CartIconProps {
-  quantity: number;
-}
+import { useCartStore } from '@/hooks/useCartStore';
 
-const CartIcon = ({ quantity }: CartIconProps) => {
+const CartIcon = () => {
+  const totalItems = useCartStore((state) => state.totalItems);
+
   return (
     <Link href='/cart' className='flex items-center gap-4'>
       <div className='relative w-8 h-8 md:w-5 md:h-5'>
         <Image src='/img/cart.png' fill alt='cart' />
       </div>
-      <span>Cart ({quantity})</span>
+      <span>Cart ({totalItems})</span>
     </Link>
   );
 };
