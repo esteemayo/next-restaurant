27b19803ace70af8489f4a019703e28ba77f62bd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-09 15:55:18 +0100

    fix: accept data via props

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-const CartIcon = () => {
+interface CartIconProps {
+  quantity: number;
+}
+
+const CartIcon = ({ quantity }: CartIconProps) => {
   return (
     <Link href='/cart' className='flex items-center gap-4'>
       <div className='relative w-8 h-8 md:w-5 md:h-5'>
         <Image src='/img/cart.png' fill alt='cart' />
       </div>
-      <span>Cart (3)</span>
+      <span>Cart ({quantity})</span>
     </Link>
   );
 };
