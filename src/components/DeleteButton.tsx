f33548bc7ac7e0c7fd37ee360796d48937fe4790
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-10 02:27:55 +0100

    fix: handle user authorization

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,8 +1,19 @@
 'use client';
 
 import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
 const DeleteButton = () => {
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <p>Loading ...</p>;
+  }
+
+  if (status === 'unauthenticated' || !session?.user.isAdmin) {
+    return;
+  }
+
   return (
     <button className='bg-red-400 p-2 rounded-full absolute top-4 right-4'>
       <Image src='/img/delete.png' width={20} height={20} alt='delete icon' />
