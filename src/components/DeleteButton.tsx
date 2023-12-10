42af1c20c7e9b56c95d7f1c2e9c6bf9872dc639c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-10 02:15:49 +0100

    feat: add DeleteButton component

---
+++ src/components/DeleteButton.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const DeleteButton = () => {
+  return (
+    <button className='bg-red-400 p-2 rounded-full absolute top-4 right-4'>
+      DeleteButton
+    </button>
+  );
+};
+
+export default DeleteButton;
