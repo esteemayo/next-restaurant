3c3d7fa7d8a058e2ad890cefb6db8e594b79a403
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-10 02:22:40 +0100

    feat: add delete icon

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import Image from 'next/image';
+
 const DeleteButton = () => {
   return (
     <button className='bg-red-400 p-2 rounded-full absolute top-4 right-4'>
-      DeleteButton
+      <Image src='/img/delete.png' width={20} height={20} alt='delete icon' />
     </button>
   );
 };
