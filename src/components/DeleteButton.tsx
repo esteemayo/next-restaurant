4a78c3ba7a72aabc9813e434f7aa56bd6c487457
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-10 02:48:07 +0100

    feat: handle delete product functionality

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -2,13 +2,33 @@
 
 import Image from 'next/image';
 import { useSession } from 'next-auth/react';
+import { useCallback } from 'react';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
 
 interface DeleteButtonProps {
-  id: string
+  id: string;
 }
 
 const DeleteButton = ({ id }: DeleteButtonProps) => {
+  const router = useRouter();
   const { data: session, status } = useSession();
+
+  const handleDelete = useCallback(
+    async (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      const res = await fetch(`http://localhost:3000/api/products/${id}`, {
+        method: 'DELETE',
+      });
+
+      if (res.status === 200) {
+        router.push('/menu');
+        toast.success('The product has been deleted!');
+      }
+    },
+    [id, router]
+  );
 
   if (status === 'loading') {
     return <p>Loading ...</p>;
@@ -19,7 +39,10 @@ const DeleteButton = ({ id }: DeleteButtonProps) => {
   }
 
   return (
-    <button className='bg-red-400 p-2 rounded-full absolute top-4 right-4'>
+    <button
+      onClick={handleDelete}
+      className='bg-red-400 p-2 rounded-full absolute top-4 right-4'
+    >
       <Image src='/img/delete.png' width={20} height={20} alt='delete icon' />
     </button>
   );
