e6d008f243502ae3526837218f3acf4fc9376ecd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-11 04:06:19 +0100

    feat: add CheckoutForm component

---
+++ src/app/pay/[id]/page.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { loadStripe, StripeElementsOptions } from '@stripe/stripe-js';
+import { useEffect, useState } from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+
+import CheckoutForm from '@/components/CheckoutForm';
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
+
+interface IParams {
+  params: {
+    id: string;
+  };
+}
+
+const Pay = ({ params }: IParams) => {
+  const { id } = params;
+
+  const [clientSecret, setClientSecret] = useState('');
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await fetch(
+          `http://localhost:3000/api/create-intent/${id}`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+
+        const data = await res.json();
+        setClientSecret(data.clientSecret);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    makeRequest();
+  }, [id]);
+
+  const options: StripeElementsOptions = {
+    clientSecret,
+    appearance: {
+      theme: 'stripe',
+    },
+  };
+
+  return (
+    <div>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+};
+
+export default Pay;
