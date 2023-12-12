f679438482c1583fb80bd247b8fa1d0b4a9088ad
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-12 04:50:44 +0100

    fix: style input elements

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -4,43 +4,58 @@ import { useSession } from 'next-auth/react';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
+const initialState = {
+  title: '',
+  desc: '',
+  price: 0,
+  catSlug: '',
+};
+
+const optionInitialState = {
+  title: '',
+  additionalPrice: 0,
+};
+
 const AddProduct = () => {
   const router = useRouter();
   const { data: session, status } = useSession();
 
-  if (status==='loading') {
+  const [inputs, setInputs] = useState(initialState);
+  const [option, setOption] = useState(optionInitialState);
+
+  if (status === 'loading') {
     return <p>Loading...</p>;
   }
 
-  if (status==='unauthenticated'||!session?.user.isAdmin) {
+  if (status === 'unauthenticated' || !session?.user.isAdmin) {
     router.push('/');
   }
 
   return (
     <div>
-      <form>
+      <form className='shadow-lg flex flex-wrap gap-4 p-8'>
         <h1 className='capitalize'>Add new product</h1>
-        <div>
+        <div className='w-full flex flex-col gap-2'>
           <label htmlFor='title'>Title</label>
-          <input type='text' name='title' />
+          <input className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200' type='text' name='title' />
         </div>
-        <div>
+        <div className='w-full flex flex-col gap-2'>
           <label htmlFor='desc'>Desc</label>
-          <textarea name='desc' />
+          <textarea className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200' name='desc' />
         </div>
-        <div>
+        <div className='w-full flex flex-col gap-2'>
           <label htmlFor='price'>Price</label>
-          <input type='number' name='price' />
+          <input className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200' type='number' name='price' />
         </div>
-        <div>
+        <div className='w-full flex flex-col gap-2'>
           <label htmlFor='category'>Category</label>
-          <input type='text' name='category' />
+          <input className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200' type='text' name='category' />
         </div>
-        <div>
+        <div className='w-full flex flex-col gap-2'>
           <label htmlFor='options'>Options</label>
           <div>
-            <input type='text' placeholder='Title' name='title' />
-            <input
+            <input className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200' type='text' placeholder='Title' name='title' />
+            <input className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200'
               type='number'
               placeholder='Addtional Price'
               name='additionalPrice'
