c3e3c5e60c17dec122ca9e9a3cee6067985e0398
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-12 03:34:21 +0100

    fix: redirect to success page after successful payment

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -57,7 +57,7 @@ const CheckoutForm = () => {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: 'http://localhost:3000',
+        return_url: 'http://localhost:3000/success',
       },
     });
 
