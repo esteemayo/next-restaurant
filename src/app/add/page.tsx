172e83cd4822ffc136968b61117df19f8f41cd1a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-12 07:48:32 +0100

    fix: add styling

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -37,34 +37,57 @@ const AddProduct = () => {
         <h1 className='capitalize'>Add new product</h1>
         <div className='w-full flex flex-col gap-2'>
           <label htmlFor='title'>Title</label>
-          <input className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200' type='text' name='title' />
+          <input
+            className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200'
+            type='text'
+            name='title'
+          />
         </div>
         <div className='w-full flex flex-col gap-2'>
           <label htmlFor='desc'>Desc</label>
-          <textarea className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200' name='desc' />
+          <textarea
+            className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200'
+            name='desc'
+          />
         </div>
         <div className='w-full flex flex-col gap-2'>
           <label htmlFor='price'>Price</label>
-          <input className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200' type='number' name='price' />
+          <input
+            className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200'
+            type='number'
+            name='price'
+          />
         </div>
         <div className='w-full flex flex-col gap-2'>
           <label htmlFor='category'>Category</label>
-          <input className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200' type='text' name='category' />
+          <input
+            className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200'
+            type='text'
+            name='category'
+          />
         </div>
         <div className='w-full flex flex-col gap-2'>
           <label htmlFor='options'>Options</label>
           <div>
-            <input className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200' type='text' placeholder='Title' name='title' />
-            <input className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200'
+            <input
+              className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200'
+              type='text'
+              placeholder='Title'
+              name='title'
+            />
+            <input
+              className='ring-1 ring-red-200 p-2 rounded-sm outline-red-300 caret-red-200'
               type='number'
               placeholder='Addtional Price'
               name='additionalPrice'
             />
-            <button className='w-52 p-2 bg-red-500 text-white capitalize rounded-sm outline-red-400 hover:bg-red-400 transition-all'>Add option</button>
+            <button className='w-52 p-2 bg-red-500 text-white capitalize rounded-sm outline-red-400 hover:bg-red-400 transition-all'>
+              Add option
+            </button>
           </div>
         </div>
         <div>
-          <div>
+          <div className='ring-1 p-2 ring-red-500 rounded-md cursor-pointer'>
             <span>Small</span>
             <span>$2</span>
           </div>
