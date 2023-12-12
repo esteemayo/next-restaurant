22df16c43f4a0294e9297785f5a3dd4e4f557b8c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-12 04:02:58 +0100

    feat: handle confirm payment logic

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,9 +1,36 @@
 'use client';
 
-const Success = () => {
-  return (
-    <div>Success</div>
-  )
-}
+import { useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
-export default Success
+const Success = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const payment_intent = searchParams.get('payment_intent');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await fetch(
+          `http://localhost:3000/api/confirm/${payment_intent}`,
+          {
+            method: 'PATCH',
+          }
+        );
+
+        router.push('/orders');
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, [payment_intent, router]);
+
+  return (
+    <div>
+      Payment successful. You are being redirected to the orders page. Please do not close the page.
+    </div>
+  );
+};
+
+export default Success;
