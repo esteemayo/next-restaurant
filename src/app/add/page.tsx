df7614dd2abed3a8c9fb651796dab51c45c0ff41
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-12 04:32:46 +0100

    fix: handle user authorization

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,8 +1,21 @@
 'use client';
 
+import { useSession } from 'next-auth/react';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const AddProduct = () => {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  if (status==='loading') {
+    return <p>Loading...</p>;
+  }
+
+  if (status==='unauthenticated'||!session?.user.isAdmin) {
+    router.push('/');
+  }
+
   return (
     <div>
       <form>
