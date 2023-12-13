81fda51766bc6b48f14e00c8bdd3fbf188ff2123
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-13 05:20:50 +0100

    fix: add styling

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -219,7 +219,7 @@ const AddProduct = () => {
           <button
             type='button'
             onClick={handleOptions}
-            className='capitalize bg-gray-500 p-2 text-white rounded-sm hover:bg-gray-400 transition-all'
+            className='capitalize bg-gray-500 p-2 text-white rounded-sm outline-gray-400 hover:bg-gray-400 transition-all'
           >
             Add option
           </button>
