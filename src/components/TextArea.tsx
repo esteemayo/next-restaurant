e74e4decc498194b6df9d0b5742cac21938ef0ba
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-14 01:55:07 +0100

    fix: add TextAreaProps type annotation

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,14 +1,14 @@
+import { TextAreaProps } from '@/types';
 
-
-const TextArea = () => {
+const TextArea = ({ name, label }: TextAreaProps) => {
   return (
     <div className='w-full flex flex-col gap-2'>
-      <label htmlFor='desc' className='text-sm'>
-        Description
+      <label htmlFor={name} className='text-sm'>
+        {label}
       </label>
       <textarea
-        id='desc'
-        name='desc'
+        id={name}
+        name={name}
         className='ring-1 ring-red-200 p-2 rounded-sm placeholder:text-red-200 outline-red-300 caret-red-200 resize-none'
       />
     </div>
