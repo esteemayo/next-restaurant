01c1d9e2a882202b7c2b532572a38dbb72046c41
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-14 03:15:20 +0100

    fix: add confirm delete dialog box

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -5,32 +5,53 @@ import { useSession } from 'next-auth/react';
 import { useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 import { toast } from 'react-toastify';
+import { withSwal } from 'react-sweetalert2';
 
 interface DeleteButtonProps {
   id: string;
+  swal: any;
 }
 
-const DeleteButton = ({ id }: DeleteButtonProps) => {
+const DeleteButton = ({ id, swal }: DeleteButtonProps) => {
   const router = useRouter();
   const { data: session, status } = useSession();
 
-  const handleDelete = useCallback(
-    async (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleDelete = useCallback(async () => {
+    const res = await fetch(`http://localhost:3000/api/products/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (res.status === 200) {
+      router.push('/menu');
+      toast.success('The product has been deleted!');
+    } else {
+      const data = await res.json();
+      toast.error(data.message);
+    }
+  }, [id, router]);
+
+  const deleteHandler = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
       e.stopPropagation();
 
-      const res = await fetch(`http://localhost:3000/api/products/${id}`, {
-        method: 'DELETE',
-      });
-
-      if (res.status === 200) {
-        router.push('/menu');
-        toast.success('The product has been deleted!');
-      } else {
-        const data = await res.json();
-        toast.error(data.message);
-      }
+      swal
+        .fire({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#6b7280',
+          cancelButtonColor: '#ef4444',
+          confirmButtonText: 'Delete',
+          reverseButtons: true,
+        })
+        .then(async (result: any) => {
+          if (result.isConfirmed) {
+            await handleDelete();
+          }
+        });
     },
-    [id, router]
+    [handleDelete, swal]
   );
 
   if (status === 'loading') {
@@ -43,7 +64,7 @@ const DeleteButton = ({ id }: DeleteButtonProps) => {
 
   return (
     <button
-      onClick={handleDelete}
+      onClick={deleteHandler}
       className='bg-red-400 p-2 rounded-full outline-red-500 absolute top-4 right-4'
     >
       <Image src='/img/delete.png' width={20} height={20} alt='delete icon' />
@@ -51,4 +72,6 @@ const DeleteButton = ({ id }: DeleteButtonProps) => {
   );
 };
 
-export default DeleteButton;
+export default withSwal(({ id, swal }: { id: string; swal: any }) => (
+  <DeleteButton id={id} swal={swal} />
+));
