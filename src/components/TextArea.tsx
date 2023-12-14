aef385a5e986c35fac85a0d4b4399e1d55de1973
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-14 01:55:49 +0100

    fix: modify

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,12 +1,13 @@
 import { TextAreaProps } from '@/types';
 
-const TextArea = ({ name, label }: TextAreaProps) => {
+const TextArea = ({ name, label, ...rest }: TextAreaProps) => {
   return (
     <div className='w-full flex flex-col gap-2'>
       <label htmlFor={name} className='text-sm'>
         {label}
       </label>
       <textarea
+        {...rest}
         id={name}
         name={name}
         className='ring-1 ring-red-200 p-2 rounded-sm placeholder:text-red-200 outline-red-300 caret-red-200 resize-none'
