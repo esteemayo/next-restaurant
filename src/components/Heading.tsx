087b68539219b74dd991b9123e900ad1c9c88b93
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-15 17:04:07 +0100

    fix: modify

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,7 @@ import { HeadingProps } from '@/types';
 
 const Heading = ({ title, subtitle, center }: HeadingProps) => {
   return (
-    <div className={center && 'text-center mb-5'}>
+    <div className={`${center && 'text-center mb-5'}`}>
       <h1 className='font-normal text-2xl text-red-500'>{title}</h1>
       <h2 className='font-light text-lg text-red-500 mt-2'>{subtitle}</h2>
     </div>
