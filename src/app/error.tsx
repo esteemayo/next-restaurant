a7c44e8db0206b471ce70c3ccf722b713ef86f77
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-15 16:55:42 +0100

    feat: add ErrorState page component

---
+++ src/app/error.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+interface ErrorStateProps {
+  message: Error;
+}
+
+const ErrorState = ({ message }: ErrorStateProps) => {
+  useEffect(() => {
+    console.error(message);
+  }, [message]);
+
+  return <div>ErrorState</div>;
+};
+
+export default ErrorState;
