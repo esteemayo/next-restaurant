1967a4fcc7e5ea24254324d4a20af46ddc41956b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-15 16:43:45 +0100

    feat: add Heading component

---
+++ src/components/Heading.tsx
@@ -0,0 +1,10 @@
+const Heading = ({ title, subtitle, center }) => {
+  return (
+    <div className="">
+      <h1 className="">{title}</h1>
+      <h2 className="">{subtitle}</h2>
+    </div>
+  );
+};
+
+export default Heading;
