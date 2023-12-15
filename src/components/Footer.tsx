14e2fb059470aa0b0086bbb15b7fc5497ee6eb72
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-15 03:12:30 +0100

    feat: add styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between uppercase'>
-      <Link href='/' className='font-bold text-xl'>
+      <Link href='/' className='font-bold text-xl outline-red-100'>
         Massimo
       </Link>
       <span>&copy; All rights reserved</span>
