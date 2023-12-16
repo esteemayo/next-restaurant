1c41930f0cf4703e7d92c75793e0697b38abf378
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-16 14:44:02 +0100

    feat: add markup and styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,18 @@
+import Image from 'next/image';
+
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <main className='p-4 lg:px-20 xl:p-40 h-[80vh]'>
+      <div className='w-full h-full flex items-center justify-center'>
+        <Image
+          src='/img/404.png'
+          width={700}
+          height={500}
+          alt='404'
+          className='block object-cover'
+        />
+      </div>
+    </main>
   );
 };
 
