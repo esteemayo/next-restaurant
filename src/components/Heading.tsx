3eb0f5e749be9753d20087300395fccad5703815
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-12-16 02:02:24 +0100

    feat: add styling

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,9 +2,9 @@ import { HeadingProps } from '@/types';
 
 const Heading = ({ title, subtitle, center }: HeadingProps) => {
   return (
-    <div className={`${center && 'text-center mb-5'}`}>
-      <h1 className='font-normal text-2xl text-red-500'>{title}</h1>
-      <h2 className='font-light text-lg text-red-500 mt-2'>{subtitle}</h2>
+    <div className={`${center ? 'text-center mb-5' : 'text-left'} text-red-500 mb-5`}>
+      <h1 className='font-normal text-2xl'>{title}</h1>
+      <h2 className='font-light text-lg mt-2'>{subtitle}</h2>
     </div>
   );
 };
